8db07c711988eea1c48af26d8d78d4a8e6b8a746
Shuang Wu <shuwu6@asu.edu>
2025-11-29 18:49:46 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"course": "IEE 305", "assignment": "HW09"}
+
+@app.get("/student/{name}")
+def read_student(name: str):
+    return {"message": f"Hello {name}!"}
+
+@app.get("/course")
+def get_course_info():
+    return {
+        "course": "IEE 305",
+        "title": "Information Systems Engineering",
+        "semester": "Fall 2025"
+    }
